1fc633a15ccc2fc7e1d219c1695e9cb4ef95be98
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-09 00:18:19 +0200

    Add login Controller

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body class="d-flex align-items-center">
 
-<div class="container-md  bg-light" style="margin-top: 2em;">
+<div class="container-md  bg-light" style="margin-top: 3em;">
     <h2>Please Login</h2>
     <form action="#" method="post" class="needs-validation" novalidate>
         @csrf
