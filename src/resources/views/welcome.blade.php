1adde7fe3cd8f679ea8e906a6cfddf0d79d325eb
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-08 22:18:08 +0200

    add modal part

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -32,8 +32,8 @@
     </style>
 </head>
 <body>
-<!-- Menu -->
 
+<!-- Menu -->
 <nav class="navbar navbar-inverse sticky-top bg-light navbar-light">
     <div class="container-fluid">
         <!-- Toggler/collapsibe Button -->
@@ -312,5 +312,25 @@
 </footer>
 <!-- End Footer -->
 
+<!-- Modal -->
+<div class="modal fade" id="ModalCenter" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalLongTitle">A little bit more about me</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
