340560c05d420f2d088c8006d84ed3f675c11f79
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-09 00:39:37 +0200

    add user data to session

--- src/app/Http/Controllers/UserAuth.php
+++ src/app/Http/Controllers/UserAuth.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use MongoDB\Driver\Session;
 
 class UserAuth extends Controller
 {
     //
     function userLogin(Request $req)
     {
-        return$req->input();
+        $data = $req->input();
+        $req->session()->put('user',$data['user']);
+        return redirect('profile');
     }
 
 }
