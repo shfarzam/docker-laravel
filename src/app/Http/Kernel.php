7b13e582c4a3e35ef28a7307ee36c811f476d87c
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-18 02:05:52 +0200

    add JWT Classes

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'AuthCheck' => \App\Http\Middleware\AuthCheck::class,
+        'AuthJWT' => \App\Http\Middleware\AuthJWT::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
