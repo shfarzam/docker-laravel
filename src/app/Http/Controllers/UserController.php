066e6ccaac6e37623908cabe45ad689a84071615
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-18 23:10:21 +0200

    define User controller

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -8,30 +8,52 @@ use Illuminate\Support\Facades\Hash;
 use \Illuminate\Http\Response;
 use \Illuminate\Support\Facades\Response as FacadeResponse;
 use \Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\JWTAuth;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * @return User[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function all() {
-        return User::all();
+        $user = $GLOBALS['userInfo'];
+        if(strcmp($user->email, $_ENV['ADMIN_USER']) == 0 ) {
+            return User::all();
+        }
+
+        return User::where('id',$user->id)->first();
     }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function addUser(Request $request) {
 
         $response = [];
-        # validate request
-        $validator = Validator::make($request->json()->all(),[
+
+        //define rules
+        $rules = [
             'first_name' => 'required',
             'last_name'=> 'required',
             'email'=>'required|email|unique:users',
             'password'=> 'required|min:6|max:20'
-        ]);
+        ];
+
+        # validate request
+        $validator = Validator::make($request->json()->all(), $rules);
+
         if($validator->fails()){
-            $response['status'] = "Fail";
-            $response['code'] = "400";
-            $response['messages'] = array();
+            $response = [
+                'Status'   => 'Fail',
+                'code'     => '400',
+                'messages' => array()
+            ];
+
             foreach ($validator->errors()->getMessages() as $item) {
                 array_push($response['messages'], $item);
             }
+
         } else {
             #insert user into databse
             $user = new User;
@@ -47,7 +69,9 @@ class UserController extends Controller
                 return response()->json(['status' => 'Something went wrong, try again later'], 400);
             }
         }
+        $code = $response['code'];
+        unset($response['code']);
 
-        return FacadeResponse::json($response);
+        return FacadeResponse::json($response,$code);
     }
 }
