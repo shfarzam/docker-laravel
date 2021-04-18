dff881d098e5de79daa488696b57c0fe6e552511
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-18 23:08:53 +0200

    add create Tables for user, product and customer

--- src/database/migrations/2021_04_17_101452_create_products_table.php
+++ src/database/migrations/2021_04_17_101452_create_products_table.php
@@ -14,10 +14,13 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigInteger('product_id')->unique();
+            $table->string('p_name');
             $table->integer('show')->default(1);
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
+
+            $table->primary(['product_id','p_name']);
         });
     }
 
