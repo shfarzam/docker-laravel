d2f1d3870243de858aedd4ff266450c896d25ca3
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-18 02:02:17 +0200

    add api Routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware'=>['AuthJWT']], function () {
+    Route::get('/users/all',[\App\Http\Controllers\UserController::class,'all']);
+    Route::post('/users',[\App\Http\Controllers\UserController::class,'addUser']);
+});
+
+Route::get('/login',function (Request $request) {
+   $credentials = $request->only(['email','password']);
+    //$token = auth()->attempt($credentials);
+    $token = auth('api')->attempt($credentials);
+
+    if (! $token) {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    return response()->json([
+        'access_token' => $token,
+        'token_type' => 'bearer',
+        'expires_in' => auth('api')->factory()->getTTL() * 60
+    ]);
+});
