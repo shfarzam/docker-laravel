2bf0468b9d0345579be9908c058eb0597c4499fb
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-25 15:17:38 +0200

    add new mysql test Driver

--- src/config/database.php
+++ src/config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'mysql_test' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_TEST_DATABASE', 'shop_test'),
+            'username' => env('DB_USERNAME', 'shah'),
+            'password' => env('DB_PASSWORD', 'admin12345'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
