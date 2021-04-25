fe44f58909322684863a1fae893c1bbb3bff0ef9
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-25 15:16:38 +0200

    add dashboard page

--- src/resources/views/admin/dashboard.blade.php
+++ src/resources/views/admin/dashboard.blade.php
@@ -1,42 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Login Page</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body class="d-flex align-items-center">
+@extends('layout.default')
+@section('title', 'Admin Dashboard')
+@section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <h4>Dashboard</h4><hr>
-            <table class="table table-hover">
-                <thead>
-                <th>Name</th>
-                <th>Email</th>
-                <th></th>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>{{ $UserInfo['first_name'].' '.$UserInfo['last_name'] }}</td>
-                    <td>{{ $UserInfo['email'] }}</td>
-                    <td><a href="{{ route('auth.logout') }}">Logout</a></td>
-                </tr>
-                </tbody>
-            </table>
+        <div class="col-md-12 col-md-offset-3">
+            <header>
+                <!-- Navbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white fixed-top">
+                    <div class="container-fluid">
 
-            <ul>
-                <li><a href="/admin/dashboard">Dashboard</a></li>
-                <li><a href="/admin/profile">Profile</a></li>
-                <li><a href="/admin/settings">Settings</a></li>
-                <li><a href="/admin/staff">Staff</a></li>
-            </ul>
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" aria-current="page" href="{{ route('admin.dashboard') }}">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.tables',['users']) }}">Users</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.tables',['customers']) }}">Customers</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.tables',['products']) }}">Products</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.tables',['orders']) }}">Orders</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('auth.logout') }}">Logout as {{ $UserInfo['first_name'].' '.$UserInfo['last_name'] }}</a>
+                                </li>
+                            </ul>
+
+                    </div>
+                </nav>
+                <!-- Navbar -->
+
+                <!-- Jumbotron -->
+                <div class="table-responsive bg-light text-center" style="margin-top: 58px;">
+                    <table class="table table-bordered table-hover  text-center ">
+                        <thead class="table-info">
+                        <tr>
+                            @foreach($columns  as $col_name => $value)
+                                <th scope="col">{{ $value }}</th>
+                            @endforeach
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @foreach($Result as $row)
+                            <tr>
+                                @foreach($columns  as $col_name => $value)
+                                    <td>
+                                        {{ $row[$value] }}
+                                    </td>
+                                @endforeach
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    <br>
+                    <span>
+                        {{ $Result->links() }}
+                    </span>
+
+                    <style>
+                        .w-5 {
+                            display: inline;
+                            width: 1.5em;
+                        }
+                    </style>
+                </div>
+                <!-- Jumbotron -->
+            </header>
+
         </div>
     </div>
 </div>
-</body>
-</html>
+
+@endsection
