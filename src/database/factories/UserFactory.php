3e86cb59c21cdacbaf47821b17edb02ed4668746
Shahriar Farzam <shahriar.farzam@l-h-de>
2021-04-26 21:51:50 +0200

    extra code clean

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -29,16 +29,10 @@ class UserFactory extends Factory
             'last_name' => $this->faker->lastName,
             'email' => 'sh.farzam@hotmail.com',
             'email_verified_at' => now(),
-            'password' => hash::make('123456'), // password
+            'password' => hash::make('admin13620722'), // password
             'remember_token' => Str::random(10),
         ];
-        return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
+
     }
 
     /**
